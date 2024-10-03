bad65b423127c69245eb457a337e34c07efd80e1
gaurav060404 <gs762282@gmail.com>
2024-10-03 18:02:39 +0530

    update taiwlind file for custom colors and fonts and add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-evenly items-center text-nav py-4 px-8'>
+      <a href="#">
+        <img src={logo} alt="Logo" className='w-28 hover:drop-shadow-md'/>
+      </a>
+      <ul className='flex items-center gap-12 text-base font-custom'>
+        <li className='p-3 hover:text-newnav cursor-pointer'>Movies</li>
+        <li className='p-3 hover:text-newnav cursor-pointer'>Series</li>
+        <li className='p-3 hover:text-newnav cursor-pointer'>Anime</li>
+        <li className='p-3 hover:text-newnav cursor-pointer'>Signup</li>
+      </ul>
+    </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import {colors as defaultColors} from 'tailwindcss/defaultTheme'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        ...defaultColors,
+        nav : "#14213d",
+        newnav : "#415a77"
+      },
+      fontFamily : {
+        custom : ["Afacad Flux"],
+      }
+    },
   },
   plugins: [],
 }
