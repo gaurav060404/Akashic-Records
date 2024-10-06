45dc6fd7d0ad4c3b1e0ba77c7a53cfed0656554f
gaurav060404 <gs762282@gmail.com>
2024-10-06 19:55:53 +0530

    add a gradient (black to transparent) over carousel images

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -14,7 +14,7 @@ export default function Carousel() {
     },8000);
 
     return () => clearInterval(interval);
-  },[url.length]);
+  },[url]);
 
   return (
     <div className='h-screen w-full relative'>
@@ -24,11 +24,12 @@ export default function Carousel() {
         alt={`Carousel ${slide}`}
         className="h-full w-full absolute inset-0 overflow-hidden object-cover bg-center duration-500 bg-blackOverlay"
         />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/85 to-transparent"></div>
         <div className='w-2/3 h-1/3 bottom-0 left-1/4 absolute flex flex-col items-center pt-8 pr-24'>
-          <h1 className='font-custom4 text-5xl font-bold pr-40'>The Records Of Akasha</h1>
+          <h1 className='font-custom4 text-5xl font-bold pr-40 text-white'>The Records Of Akasha</h1>
           <div className='w-2/3 pt-2 pl-20'>
-            <p className='pl-3'>To Keep All The Records Of Your </p>
-            <p>Favourite Movies , TV Shows And Animes.</p>
+            <p className='pl-7 text-white'>To Keep All The Records Of Your </p>
+            <p className='text-white'>Favourite Movies , TV Shows And Animes.</p>
             <button className='rounded-sm bg-white font-custom4 hover:bg-green-500 hover:text-white cursor-pointer px-3 py-2 ml-24 mt-5 mb-2 border-none border-green-500 duration-300'>Click Here!!!</button>
           </div>
         </div>
