55e6a3dcf63e6fbafe148534de74adc9fd945655
gaurav060404 <gs762282@gmail.com>
2024-10-15 22:25:37 +0530

    fixed image fecthing as fetched images felt like distorted

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({posterPath,posterName}) {
     <div className='h-full w-48 bg-black flex-row justify-center items-center pt-4'>
       <div className='w-full h-full flex-row justify-center items-center group'>
         <div className='bg-white h-5/6 rounded-sm overflow-hidden'>
-          <img src={`https://image.tmdb.org/t/p/original${posterPath}`} alt="" className='object-cover w-full h-full hover:border cursor-pointer' />
+          <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`} alt="" className='object-cover w-full h-full hover:border cursor-pointer' />
         </div>
         <p className='text-white font-custom4 text-xs text-center py-5 group-hover:text-blue-400 cursor-pointer'>{posterName}</p>
       </div>
