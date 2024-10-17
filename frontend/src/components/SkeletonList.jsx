e9ecf4dbd2ff1fca6dbe9ce20220baf2ee560874
gaurav060404 <gs762282@gmail.com>
2024-10-17 18:33:48 +0530

    Refactor SkeletonList component to adjust the width and height of the skeleton elements for better visual alignment and added skeletons on previous commit .

--- frontend/src/components/SkeletonList.jsx
+++ frontend/src/components/SkeletonList.jsx
@@ -6,14 +6,14 @@ const SkeletonList = () => {
   return (
     <div className='bg-black h-96 flex-row'>
       <div className='w-full text-white flex justify-between items-center px-24 pt-4'>
-        <Skeleton width={200} height={30} />
-        <Skeleton width={100} height={20} />
+        <Skeleton width={150} height={30} />
+        <Skeleton width={50} height={20} />
       </div>
       <div className='bg-black h-80 flex justify-evenly'>
         {Array(5).fill().map((_, index) => (
-          <div key={index} className='w-1/5 p-2'>
-            <Skeleton height={300} />
-            <Skeleton height={20} />
+          <div key={index} className='w-1/6 p-1'>
+            <Skeleton height={275} />
+            <Skeleton height={15} />
           </div>
         ))}
       </div>
