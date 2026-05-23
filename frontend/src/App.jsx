ec5d4754f28157cc6c73865c6b2b46d013f9bdbb
gaurav060404 <gs762282@gmail.com>
2026-05-23 21:48:37 +0530

    add react query and removing the recoil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Movies from './pages/Movies'
@@ -15,17 +15,22 @@ import Profile from "./pages/Profile";
 import { Toaster } from 'react-hot-toast';
 import About from './pages/About.jsx';
 import FAQ from './pages/FAQ.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
     const userData = urlParams.get('user');
-    
+
     if (token) {
       try {
         localStorage.setItem('token', token);
-        
+
         if (userData) {
           const user = JSON.parse(decodeURIComponent(userData));
           const userDataToStore = {
@@ -38,7 +43,7 @@ export default function App() {
           // If no user data provided, fetch it from the backend
           fetchUserData(token);
         }
-        
+
         // Clean URL
         window.history.replaceState({}, document.title, window.location.pathname);
       } catch (error) {
@@ -55,7 +60,7 @@ export default function App() {
           Authorization: `Bearer ${token}`
         }
       });
-      
+
       const userData = {
         name: response.data.name,
         avatar: response.data.avatar || response.data.picture,
@@ -69,54 +74,56 @@ export default function App() {
   };
 
   return (
-    <BrowserRouter>
-    <RecoilRoot>
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='/signup' element={<Signup/>} ></Route>
-        <Route path='/login' element={<Login/>} ></Route>
-        <Route path='/anime' element={<Anime/>} ></Route>
-        <Route path='/movies' element={<Movies/>} ></Route>
-        <Route path='/series' element={<Series/>} ></Route>
-        <Route path='/:title/:id' element={<Details/>} ></Route>
-        <Route path='/:category/rated/:id/:posterName' element={<Details/>} ></Route>
-        <Route path='/google/callback' element={<GoogleCallback />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path='*' element={<NotFound/>}></Route>
-      </Routes>
-      <Toaster 
-        position="top-center"
-        reverseOrder={false}
-        gutter={8}
-        toastOptions={{
-          // Default options for all toasts
-          duration: 5000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-          // Different styles based on toast type
-          success: {
-            duration: 3000,
-            iconTheme: {
-              primary: '#10B981',
-              secondary: 'white',
-            },
-          },
-          error: {
-            duration: 4000,
-            iconTheme: {
-              primary: '#EF4444',
-              secondary: 'white',
-            },
-          },
-        }}
-      />
-    </div>
-    </RecoilRoot>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <RecoilRoot>
+          <div>
+            <Routes>
+              <Route path='/' element={<Home />} ></Route>
+              <Route path='/signup' element={<Signup />} ></Route>
+              <Route path='/login' element={<Login />} ></Route>
+              <Route path='/anime' element={<Anime />} ></Route>
+              <Route path='/movies' element={<Movies />} ></Route>
+              <Route path='/series' element={<Series />} ></Route>
+              <Route path='/:title/:id' element={<Details />} ></Route>
+              <Route path='/:category/rated/:id/:posterName' element={<Details />} ></Route>
+              <Route path='/google/callback' element={<GoogleCallback />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path='*' element={<NotFound />}></Route>
+            </Routes>
+            <Toaster
+              position="top-center"
+              reverseOrder={false}
+              gutter={8}
+              toastOptions={{
+                // Default options for all toasts
+                duration: 5000,
+                style: {
+                  background: '#363636',
+                  color: '#fff',
+                },
+                // Different styles based on toast type
+                success: {
+                  duration: 3000,
+                  iconTheme: {
+                    primary: '#10B981',
+                    secondary: 'white',
+                  },
+                },
+                error: {
+                  duration: 4000,
+                  iconTheme: {
+                    primary: '#EF4444',
+                    secondary: 'white',
+                  },
+                },
+              }}
+            />
+          </div>
+        </RecoilRoot>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
--- frontend/src/pages/Anime.jsx
+++ frontend/src/pages/Anime.jsx
@@ -2,14 +2,33 @@ import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import List from '../components/List';
 import SkeletonList from '../components/SkeletonList';
-import { useRecoilValueLoadable } from 'recoil';
-import { animeSelector, topRatedAnimes } from '../store/store';
 import SkeletonRated from '../components/SkeletonRated';
 import Rated from '../components/Rated';
+import { useQuery } from '@tanstack/react-query';
+import {
+  trendingAnimes,
+  topRatedAnimes,
+} from '../services/animeService.js';
 
 export default function Anime() {
-  const animeLoadable = useRecoilValueLoadable(animeSelector);
-  const ratedAnimes = useRecoilValueLoadable(topRatedAnimes);
+
+  const {
+    data: trendingData,
+    isLoading: trendingLoading,
+    isError: trendingError,
+  } = useQuery({
+    queryKey: ['trending-animes'],
+    queryFn: trendingAnimes,
+  });
+
+  const {
+    data: topRatedData,
+    isLoading: topRatedLoading,
+    isError: topRatedError,
+  } = useQuery({
+    queryKey: ['top-rated-animes'],
+    queryFn: topRatedAnimes,
+  });
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,21 +37,44 @@ export default function Anime() {
   return (
     <div className='w-full h-full absolute bg-black'>
       <div className='w-full h-20'>
-        < Navbar isHomePage={false} hasBg={false} />
+        <Navbar isHomePage={false} hasBg={false} />
       </div>
-      {animeLoadable.state === 'loading' && <SkeletonList title="Anime" />}
-      {animeLoadable.state === 'hasValue' && (
-        <List title="Anime" poster={animeLoadable.contents} isPopular={false} isAnime={true}/>
+
+      {/* Trending Anime */}
+      {trendingLoading && <SkeletonList title="Anime" />}
+
+      {trendingData && (
+        <List
+          title="Anime"
+          poster={trendingData}
+          isPopular={false}
+          isAnime={true}
+        />
       )}
-      {animeLoadable.state === 'hasError' && (
-        <div className='text-white'>Error loading data</div>
+
+      {trendingError && (
+        <div className='text-white'>
+          Error loading anime data
+        </div>
       )}
-      {ratedAnimes.state === 'loading' && <SkeletonRated title={"Anime"} isRated={true} />}
-      {ratedAnimes.state === 'hasValue' && (
-        <Rated title="Anime" isRated={true} rated={ratedAnimes.contents} />
+
+      {/* Top Rated Anime */}
+      {topRatedLoading && (
+        <SkeletonRated title="Anime" isRated={true} />
       )}
-      {ratedAnimes.state === 'hasError' && (
-        <div className='text-white'>Error loading data</div>
+
+      {topRatedData && (
+        <Rated
+          title="Anime"
+          isRated={true}
+          rated={topRatedData}
+        />
+      )}
+
+      {topRatedError && (
+        <div className='text-white'>
+          Error loading top rated anime
+        </div>
       )}
     </div>
   );
