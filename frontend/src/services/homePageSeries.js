f29b2cde604a186fb7a8883125e6a2061ab55c78
gaurav060404 <gs762282@gmail.com>
2026-05-24 14:03:45 +0530

    fix return data

--- frontend/src/services/homePageSeries.js
+++ frontend/src/services/homePageSeries.js
@@ -1,16 +1,16 @@
 import { axiosInstance } from "./api.js";
 
 export const trending = async () => {
-    const response = await axiosInstance.get('/trending');
-    return response.data;
+  const response = await axiosInstance.get("/trending");
+  return response.data.data;
 };
 
 export const carouselPosters = async () => {
-    const response = await axiosInstance.get('/posters');
-    return response.data;
+  const response = await axiosInstance.get("/posters");
+  return response.data.data;
 };
 
 export const topCreators = async () => {
-    const response = await axiosInstance.get('/top-creators');
-    return response.data;
+  const response = await axiosInstance.get("/top-creators");
+  return response.data.data;
 };
